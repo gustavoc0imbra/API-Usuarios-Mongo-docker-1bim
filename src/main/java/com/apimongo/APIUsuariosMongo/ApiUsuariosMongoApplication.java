0b9f602b602984eaf27839291ad714052ce702ff
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-04-05 10:53:16 -0300

    Swagger dependency

--- src/main/java/com/apimongo/APIUsuariosMongo/ApiUsuariosMongoApplication.java
+++ src/main/java/com/apimongo/APIUsuariosMongo/ApiUsuariosMongoApplication.java
@@ -1,9 +1,12 @@
 package com.apimongo.APIUsuariosMongo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Users API - MongoDB", version = "1.0", description = "API that receives user data and store in mongodb"))
 public class ApiUsuariosMongoApplication {
 
 	public static void main(String[] args) {
