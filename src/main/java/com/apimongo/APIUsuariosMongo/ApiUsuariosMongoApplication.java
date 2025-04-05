5567909e8f137b5003593a43d1bbce8e5ebe79bf
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-04-05 09:55:56 -0300

    Init Project

---
+++ src/main/java/com/apimongo/APIUsuariosMongo/ApiUsuariosMongoApplication.java
@@ -0,0 +1,13 @@
+package com.apimongo.APIUsuariosMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsuariosMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUsuariosMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apimongo/APIUsuariosMongo/ApiUsuariosMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimongo.APIUsuariosMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
