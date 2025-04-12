aa67f50cad3dbf816f434a5a0a7335111d24154f
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-04-12 12:04:48 -0300

    standardize API's URL

--- src/main/java/com/apimongo/APIUsuariosMongo/constants/Constants.java
+++ src/main/java/com/apimongo/APIUsuariosMongo/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.apimongo.APIUsuariosMongo.constants;
 
 public class Constants {
-    public static final String API_URL = "/API/V0";
+    public static final String API_URL = "/api/v0";
     public static final String API_USERS = API_URL + "/users";
 }
